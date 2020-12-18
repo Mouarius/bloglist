c91db4aff776bb7d2a4e9c422fc36a8ec76d2d4a
mouarius <mouarius@gmail.com>
2020-12-18 12:57:25 +0100

    conditions if testing

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,11 @@
 require('dotenv').config()
 
 const { PORT } = process.env
-const { MONGO_URL } = process.env
+let { MONGO_URL } = process.env
+
+if (process.env.NODE_ENV === 'test') {
+  MONGO_URL = process.env.TEST_MONGO_URL
+}
 
 module.exports = {
   PORT,
