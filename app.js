2f6e1439655638168bed83829b762d286b1b796a
mouarius <mouarius@gmail.com>
2021-01-05 10:55:22 +0100

    adjusment for cypress testing

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const config = require('./utils/config')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
+const testingRouter = require('./controllers/testing')
 const middleware = require('./utils/middleware')
 
 // morgan.token('content', (req) => JSON.stringify(req.body))
@@ -37,6 +38,10 @@ app.use(middleware.tokenExtractor)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+
+if (process.env.NODE_ENV === 'test') {
+  app.use('/api/testing', testingRouter)
+}
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
